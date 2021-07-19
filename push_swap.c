98c1e07f5964befaf7ded7ce3c9161648fd156fe
isaaclim <isaaclimjj@yahoo.com>
2021-07-20 01:01:01 +0800

    split main to ft_createStack

--- push_swap.c
+++ push_swap.c
@@ -40,100 +40,49 @@ bool	ft_check_input(char *argv, t_list *lst)
 		return (true);
 }
 
-/*
-** Find the number in stack_a where stack_b top number should be placed 
-**	on top of (it is next number bigger than stack_b top number)
-*/
-int	ft_onTopOf_ascending(t_list *lst_to, int num_from)
+static void	ft_createStack(t_list **stack_a, char **argv)
 {
-	int		onTopOf;
-	int		smallest;
-	int		stack_ori;
-	t_list	*temp;
-	
-	stack_ori = lst_to->num;
-	smallest = ft_smallest(lst_to);
-	while (lst_to->num != smallest)
-		ft_sort_rotate(&lst_to);
-	temp = lst_to;
-	while (temp && num_from > temp->num)
-		temp = temp->next;
-	if (temp)
-		onTopOf = temp->num;
-	else
-		onTopOf = smallest;
-	while (lst_to->num != stack_ori)
-		ft_sort_rotate_rev(&lst_to);
-	return (onTopOf);
-}
+	t_list	*new;
+	int		i;
 
-int	ft_onTopOf_decending(t_list *lst_to, int num_from)
-{
-	int		onTopOf;
-	int		largest;
-	int		stack_ori;
-	t_list	*temp;
-	
-	stack_ori = lst_to->num;
-	largest = ft_largest(lst_to);
-	while (lst_to->num != largest)
-		ft_sort_rotate(&lst_to);
-	temp = lst_to;
-	while (temp && num_from < temp->num)
-		temp = temp->next;
-	if (temp)
-		onTopOf = temp->num;
-	else
-		onTopOf = largest;
-	while (lst_to->num != stack_ori)
-		ft_sort_rotate_rev(&lst_to);
-	return (onTopOf);
+	i = 0;
+	while (argv[++i])
+	{
+		if (!(ft_check_input(argv[i], *stack_a)))
+		{
+			ft_putstr_fd("Error\n", 2);
+			ft_lstclear(stack_a);
+			exit (0);
+		}
+		new = ft_lstnew(ft_atoi(argv[i]));
+		ft_lstadd_back(stack_a, new);
+	}
 }
-
 int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
-	t_list	*new;
-	int		i;
 
 	if (argc < 2)
 		return (0);
 	stack_a = NULL;
 	stack_b = NULL;
-	i = 0;
-	while (argv[++i])
-	{
-		if (!(ft_check_input(argv[i], stack_a)))
-		{
-			ft_putstr_fd("Error\n", 2);
-			ft_lstclear(&stack_a);
-			return (0);
-		}
-		new = ft_lstnew(ft_atoi(argv[i]));
-		ft_lstadd_back(&stack_a, new);
-	}
-	ft_putstr_fd("\nStack_a:\t", 1);
-	ft_putlst(stack_a);
-	ft_putchar_fd('\n', 1);
+	ft_createStack(&stack_a, argv);
 	if (!(ft_is_sorted(stack_a)))
 	{
-		// ft_sort_radix(&stack_a, &stack_b);
-		// if (ft_lstsize(stack_a) <= 3)
-		// 	ft_sort_xs(&stack_a, &stack_b);
-		// else if (ft_lstsize(stack_a) <= 5)
-		// 	ft_sort_s(&stack_a, &stack_b);
-		// else if (ft_lstsize(stack_a) <= 100)
-			ft_sort_m(&stack_a, &stack_b);
-		// else
-			// ft_sort_insertion(&stack_a, &stack_b);
+		if (ft_lstsize(stack_a) <= 3)
+			ft_sort_xs(&stack_a, &stack_b);
+		else if (ft_lstsize(stack_a) <= 5)
+			ft_sort_s(&stack_a, &stack_b);
+		else
+			ft_sort_radix(&stack_a, &stack_b);
 	}
-	ft_putstr_fd("Stack_a:\t", 1);
-	ft_putlst(stack_a);
-	ft_putstr_fd("\nStack_b:\t", 1);
-	ft_putlst(stack_b);
-
-	ft_putchar_fd('\n', 1);
 	ft_lstclear(&stack_a);
 	ft_lstclear(&stack_b);
 }
+
+	// ft_putstr_fd("Stack_a:\t", 1);
+	// ft_putlst(stack_a);
+	// ft_putstr_fd("\nStack_b:\t", 1);
+	// ft_putlst(stack_b);
+	// ft_putchar_fd('\n', 1);
