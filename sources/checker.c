312d28b2c380e394a4742cf8d58738717aa3d9a5
isaaclim <isaaclimjj@yahoo.com>
2021-07-30 22:59:07 +0800

    checker.c can accept inputs

--- sources/checker.c
+++ sources/checker.c
@@ -1,13 +1,26 @@
 #include "push_swap.h"
+#include "get_next_line.h"
 // # include <stdio.h> //REMOVE
 
 int main(int argc, char **argv)
 {
-	while (1)
-	;
-	for (int i = 1; i <= argc; i++)
+	int i;
+	char *line;
+	
+	for (int j = 1; j < argc; j++)
 	{
-		printf("%s, ", argv[i]);
+		printf("%s ", argv[j]);
 	}
-	printf("\n");
+
+	i = 0;
+	while (get_next_line(0, &line))
+	{
+		i++;
+		printf("\nGNL count: %d\n", i);
+		free(line);
+	}
+	// for (int i = 1; i <= argc; i++)
+	// {
+	// 	printf("%s, ", argv[i]);
+	// }
 }
