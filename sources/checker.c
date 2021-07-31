69c86f9051cf596a2348122ec56dcc338715c5e1
isaaclim <isaaclimjj@yahoo.com>
2021-07-31 20:20:32 +0800

    checker.c good

--- sources/checker.c
+++ sources/checker.c
@@ -1,9 +1,9 @@
 #include "push_swap.h"
 #include "get_next_line.h"
 
-bool	ft_modifyList(char *line, t_list **stack_a, t_list **stack_b)
+static bool	ft_modifyList(char *line, t_list **stack_a, t_list **stack_b)
 {
-	if(!(ft_strnstr("ssasbpapbrrrarrb", line, 16)))
+	if (!(ft_strnstr("ssasbpapbrrrarrb", line, 16)))
 		return (false);
 	if (!(ft_strncmp("sa", line, 2) && ft_strncmp("ss", line, 2)))
 		ft_sort_swap(stack_a);
@@ -21,45 +21,34 @@ bool	ft_modifyList(char *line, t_list **stack_a, t_list **stack_b)
 		ft_sort_rotate_rev(stack_a);
 	if (!(ft_strncmp("rrb", line, 3) && ft_strncmp("rrr", line, 3)))
 		ft_sort_rotate_rev(stack_b);
-		// printf("YES: %s\n", line);
-	// else
-	// 	ft_sort(line, stack_a, stack_b);
 	return (true);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_list *stack_a;
-	t_list *stack_b;
-	int i;
-	char *line;
-	
+	t_list	*stack_a;
+	t_list	*stack_b;
+	char	*line;
+	bool	error;
+
 	if (argc < 2)
 		return (0);
 	stack_a = NULL;
 	stack_b = NULL;
 	ft_createStack(&stack_a, argv);
-	i = 0;
 	while (get_next_line(0, &line))
 	{
 		if (!(ft_modifyList(line, &stack_a, &stack_b)))
-			break;
+			error = true;
 		free(line);
-		i++;
 	}
 	free(line);
-	if (ft_is_sorted(stack_a) && !stack_b)
-		write(1, "OK\n", 3);
+	if (error)
+		ft_putstr_fd("Error\n", 2);
+	else if (ft_is_sorted(stack_a) && !stack_b)
+		ft_putstr_fd("OK\n", 2);
 	else
-		write(1, "KO\n", 3);
-
-	printf("\nGNL count: %d\n", i);
-	ft_putstr_fd("Stack_a:\t", 1);
-	ft_putlst(stack_a);
-	ft_putstr_fd("\nStack_b:\t", 1);
-	ft_putlst(stack_b);
-	ft_putchar_fd('\n', 1);
-
+		ft_putstr_fd("KO\n", 2);
 	ft_lstclear(&stack_a);
 	ft_lstclear(&stack_b);
 }
