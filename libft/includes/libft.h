3d92bbaeab42a34481d96c1c27c2d8f2bca912cc
isaaclim <isaaclimjj@yahoo.com>
2021-08-03 15:30:07 +0800

    libft added ft_split and ft_memcpy

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -13,9 +13,11 @@ typedef struct s_list
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
+char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
